a07116dca4e554852270696946f2e25eea7cbefa
Davlatov Ibroghim <ibrohimjonnn.77>
2024-12-23 08:43:54 +0500

    day3

---
+++ main.js
@@ -0,0 +1,74 @@
+//task 1
+
+// function myfunc(a){
+// return a[0]
+// }
+// console.log(myfunc([1, 2, 3]));
+
+//task2
+
+// function myfunc(...a){
+//    return a.map((e) => {return e
+//    });
+// }
+// console.log(myfunc(1, 2));
+
+//task3
+
+// function myfunc(a){
+// return a.toReversed()
+// }
+// console.log(myfunc([1, 2, 3, 4]));
+
+//task4
+// function myfunc(a){
+//    return a.map((e) => e + 1)
+// }
+// console.log(myfunc([0, 1, 2, 3]));
+
+//task5
+
+// function myfunc(a){
+//     return a[a.length - 1]
+// }
+// console.log(myfunc([1, 2, 3]));
+
+//task6
+// function myfunc(a){
+//     return a.join(" ")
+// }
+// console.log(myfunc(["a", "b", "c", "d", "e", "f"]));
+
+// task7
+
+// function mul(a, b) {
+//   let cnt = 0;
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] == b) {
+//       cnt++;
+//     }
+//   }
+//   return cnt > 0 ? true : false;
+// }
+// console.log(mul([1, 2, 3, 4, 5, 6, 7], 2));
+
+//8task
+
+// function myfunc(a,b){
+// return a.indexOf(b);
+// }
+// console.log(myfunc(["hi", "edabit", "fgh", "abc"], "fgh"));
+
+//9task 
+
+// function myfunc(...a){
+//       return a.reduce((acc, e) => acc.concat(e), [])
+// }
+// console.log(myfunc([1,2], [3,4]))
+
+//task10
+
+// function myfunc(a){
+//     return a.join(" ")
+// }
+// console.log(myfunc(["9.2", "4.2"]));
